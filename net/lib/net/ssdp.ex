caccbbfa1d11123574f4f2d6b308c1361279566e
Jean-Luc Geering <jeanluc.geering@ufirstgroup.com>
2018-01-20 23:39:12 +0100

    fix indentation

--- net/lib/net/ssdp.ex
+++ net/lib/net/ssdp.ex
@@ -1,17 +1,17 @@
 defmodule Net.SSDP do
 
-	def discover(:hue_bridges) do
-		Nerves.SSDPClient.discover(target: "urn:schemas-upnp-org:device:basic:1", seconds: 5)
-		|> Enum.filter(fn {_k, v} ->
-			Map.has_key?(v, :"hue-bridgeid") && Map.has_key?(v, :host)
-		end)
-		|> Enum.map(fn {_k, v} -> Map.get(v, :host) end)
-		|> Enum.reject(&is_nil/1)
-		|> Enum.uniq()
-	end
+  def discover(:hue_bridges) do
+    Nerves.SSDPClient.discover(target: "urn:schemas-upnp-org:device:basic:1", seconds: 5)
+    |> Enum.filter(fn {_k, v} ->
+      Map.has_key?(v, :"hue-bridgeid") && Map.has_key?(v, :host)
+    end)
+    |> Enum.map(fn {_k, v} -> Map.get(v, :host) end)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.uniq()
+  end
 
-	def discover(:sonos) do
-		{:error, "not implemented yet"}
-	end
+  def discover(:sonos) do
+    {:error, "not implemented yet"}
+  end
 
 end
