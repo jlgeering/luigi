ee68450acec069a57b6bec768e7b4a95b5bca566
Jean-Luc Geering <jlgeering.13@gmail.com>
2018-01-22 08:36:33 +0100

    cleanup

--- ui/lib/ui/application.ex
+++ ui/lib/ui/application.ex
@@ -8,10 +8,7 @@ defmodule Ui.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the endpoint when the application starts
       supervisor(UiWeb.Endpoint, []),
-      # Start your own worker by calling: Ui.Worker.start_link(arg1, arg2, arg3)
-      # worker(Ui.Worker, [arg1, arg2, arg3]),
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
